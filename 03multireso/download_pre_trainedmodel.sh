62926f1d6fad3a668b64c667f6fc46f1aa7fc1bb
zlin0 <zhanglin@nii.ac.jp>
2023-02-06 01:54:23 +0900

    refresh git

--- 03multireso/download_pre_trainedmodel.sh
+++ 03multireso/download_pre_trainedmodel.sh
@@ -5,7 +5,7 @@ FILE_NAMEs=(multi-reso single-reso)
 for file in ${FILE_NAMEs}; do
     link="https://zenodo.org/record/6674660/files/"${file}".tar.gz?download=1"
     if [ ! -e ./${file}/01/trained_network.pt ]; then
-        echo -e "${RED}Downloading pretrained models forPartialSpoof"
+        echo -e "${RED}Downloading pretrained models for PartialSpoof"
         echo ${link}
         wget -q --show-progress -c ${link} -O  ${file}.tar.gz
     fi
